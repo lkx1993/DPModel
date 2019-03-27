6a784d0f41a4f550e5cdfb6d98a020ca9e20bea9
夏浅 <769298218@qq.com>
2019-03-27 21:31:13 +0800

    update:测试jdk动态代理,生成.class

--- src/dp/com/company/结构型模式/a06_proxy/dynamic/section2/Client.java
+++ src/dp/com/company/结构型模式/a06_proxy/dynamic/section2/Client.java
@@ -8,6 +8,10 @@ package com.company.结构型模式.a06_proxy.dynamic.section2;
 public class Client {
 	
 	public static void main(String[] args) {
+		//https://blog.csdn.net/lovejj1994/article/details/78080124
+		//生成代理类文件,路径是项目根路径下的com.sun.proxy
+		System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+
 		//定义一个主题
 		Subject subject = new RealSubject();
 		//定义主题的代理
